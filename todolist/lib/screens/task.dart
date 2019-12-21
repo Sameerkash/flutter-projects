937e2ae363b8c929d50eb27823ae12dc913fe642
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2019-12-21 17:52:05 +0530

    UI complete

---
+++ todolist/lib/screens/task.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import '../widgets/task_list.dart';
+
+class TaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+              top: 90.0,
+              left: 30.0,
+              right: 30.0,
+              bottom: 30.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(
+                    Icons.list,
+                    size: 30,
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "ToDoey",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  "12 tasks",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
+              ),
+              child: TaskList(),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: buildBottomSheet,
+          );
+        },
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget buildBottomSheet(BuildContext context) {
+    return Container(
+      color: Color(0xff3757575),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(
+              20,
+            ),
+          ),
+        ),
+        padding: EdgeInsets.fromLTRB(40, 60, 40, 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              "Add a Task",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 45,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            SizedBox(height: 50,),
+            TextField(
+              textAlign: TextAlign.center ,
+              autofocus: true,
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            FlatButton(
+              color: Colors.lightBlueAccent,
+              child: Text(
+                "Add",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ todolist/lib/widgets/task_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import './task_tile.dart';
+
+class TaskList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[TaskTile(), TaskTile(), TaskTile(), TaskTile()],
+    );
+  }
+}
