b32e456229726a2fc0ab08eea3c1d149fae7a294
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-05-28 20:25:48 +0530

    responsive web

--- flutter_web/lib/main.dart
+++ flutter_web/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './views/home/home_view.dart';
+import 'package:flutter_web/views/home/home_view.dart';
 
 void main() => runApp(MyApp());
 
--- flutter_web/lib/views/home/home_view.dart
+++ flutter_web/lib/views/home/home_view.dart
@@ -1,33 +1,33 @@
 import 'package:flutter/material.dart';
-import '../../widgets/call_to_action/call_to_action.dart';
-import '../../widgets/centered_view/centered_view.dart';
-import '../../widgets/course_details/course_details.dart';
-import '../../widgets/navigation_bar/navigation_bar.dart';
+import 'package:flutter_web/views/home/home_content_desktop.dart';
+import 'package:flutter_web/views/home/home_content_mobile.dart';
+import 'package:flutter_web/widgets/centered_view/centered_view.dart';
+import 'package:flutter_web/widgets/navigation_bar/navigation_bar.dart';
+import 'package:flutter_web/widgets/navigation_drawer/navigation_drawer.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: CenteredView(
-        child: Column(
-          children: <Widget>[
-            NavigationBar(),
-            Expanded(
-              child: Row(
-                children: <Widget>[
-                  CourseDetails(),
-                  Expanded(
-                    child: Center(
-                      child: CallToAction('Join Course'),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) => Scaffold(
+        drawer: sizingInformation.deviceScreenType == DeviceScreenType.mobile
+            ? NavigationDrawer()
+            : null,
+        backgroundColor: Colors.white,
+        body: CenteredView(
+          child: Column(
+            children: <Widget>[
+              NavigationBar(),
+              Expanded(
+                  child: ScreenTypeLayout(
+                mobile: HomeContentMobile(),
+                desktop: HomeContentDesktop(),
+              ))
+            ],
+          ),
         ),
       ),
     );
