400d8e36f00561364ecd5cafe238d4c73fdc7e20
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-06-20 11:52:51 +0530

    init

--- flutter_flame/lib/main.dart
+++ flutter_flame/lib/main.dart
@@ -1,20 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flame/util.dart';
-import 'package:flutter/services.dart';
-import 'langaw-game.dart';
-import 'package:flame/gestures.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  Util flameUtil = Util();
-  await flameUtil.fullScreen();
-  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
-
-  LangawGame game = LangawGame();
-  runApp(game.widget);
-
-  TapGestureRecognizer tapper = TapGestureRecognizer();
-  tapper.onTapDown = game.onTapDown;
-  flameUtil.addGestureRecognizer(tapper); 
-}
+void main() async {}
