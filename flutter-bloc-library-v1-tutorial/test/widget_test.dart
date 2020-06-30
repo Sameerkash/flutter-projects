bc55ceb737ac8d2db1415525d7e4882389fa8794
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-06-30 21:20:32 +0530

    bloc init

--- flutter-bloc-library-v1-tutorial/test/widget_test.dart
+++ flutter-bloc-library-v1-tutorial/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_ar_core/main.dart';
+import 'package:flutter_bloc_v1_tutorial/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
